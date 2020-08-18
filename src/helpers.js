8518ee9bca59a013814867411e0a3cefec785f82
Mattia Panzeri <mattia.panzeri93@gmail.com>
2020-08-18 14:25:28 +0200

    🎨 Lint and Prettify module code

--- src/helpers.js
+++ src/helpers.js
@@ -1,7 +1,8 @@
 export function isImage(file) {
-    if (file.type.split('/')[0] === 'image') {
-        return true;
-    }
+  if (file.type.split('/')[0] === 'image') {
+    return true;
+  }
+  return false;
 }
 
 export function convertBytesToMbsOrKbs(filesize) {
@@ -17,23 +18,23 @@ export function convertBytesToMbsOrKbs(filesize) {
 }
 
 export async function createFileFromUrl(url) {
-    const response = await fetch(url);
-    const data = await response.blob();
-    const metadata = {type: data.type};
-    const filename = url.replace(/\?.+/, '').split('/').pop();
-    return new File([data], filename, metadata);
+  const response = await fetch(url);
+  const data = await response.blob();
+  const metadata = { type: data.type };
+  const filename = url.replace(/\?.+/, '').split('/').pop();
+  return new File([data], filename, metadata);
 }
 
 export function readFile(file) {
-    return new Promise((resolve, reject) => {
-        const reader = new FileReader();
-        reader.onload = (event) => {
-            resolve(event?.target?.result);
-        };
-        reader.onerror = (event) => {
-            reader.abort();
-            reject(event);
-        };
-        reader.readAsDataURL(file);
-    });
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onload = (event) => {
+      resolve(event?.target?.result);
+    };
+    reader.onerror = (event) => {
+      reader.abort();
+      reject(event);
+    };
+    reader.readAsDataURL(file);
+  });
 }
